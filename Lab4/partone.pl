dcbdeb09f1cabc6042d86ed42265c67e7d3bfb82
Jay <sara@Jays-MacBook-Pro.local>
2017-10-29 22:55:09 -0400

    Adding lab 4 index with Perl program

---
+++ Lab4/partone.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use CGI':standard';
+use strict;
+use warnings;
+
+print "Content-type: text/html\n\n";
+
+
+print "<head>\n";
+    print "<link href = 'stylesheet.css' rel = 'stylesheet'/>\n";
+    print "<title>CPS530-Lab4Part2</title>\n";
+print "</head>\n";
+
+print "<body>\n";
+	my $first = param ('first');
+	my $last = param ('last');
+	my $year = param ('year');
+	my $location = param ('location');
+
+	print "Hello $first $last, you are in year $year, and your home town is $location";
+	print "By: Sara J. Jahanzad - 500 748 291";
+print "</body>\n";
